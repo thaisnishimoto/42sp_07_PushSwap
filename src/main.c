1065a2b815e6e4e32a55eff5e4696f4fb8676db0
Thais Nishimoto <thaismnishimoto@gmail.com>
2023-10-20 14:15:11 -0300

    temp

--- src/main.c
+++ src/main.c
@@ -6,12 +6,27 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 11:53:26 by tmina-ni          #+#    #+#             */
-/*   Updated: 2023/10/18 21:18:06 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2023/10/20 14:13:40 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+char	**parse_argv(int argc, char *argv[])
+{
+	char	**temp;
+	int	i;
+
+	i = 0;
+	while (i < argc - 1)
+	{
+		
+	}
+	else if (argc == 2)
+		num_list = ft_split(argv[1], ' ');
+	else if (argc > 2)
+		num_list = argv[1];
+}
 
 int	ft_valid_atoi(const char *nptr)
 {
@@ -51,11 +66,12 @@ void	generate_stacks(t_stack *a, t_stack *b, char **num_list)
 	b->maxsize = count;
 	b->top = 0;
 	b->items = ft_calloc(count, sizeof(int));
-	i = count - 1;
-	while (i >= 0)
+	i = 0;
+	while (count > 0)
 	{
-		a[i] = ft_valid_atoi(&argv[i]);
-		i--;
+		a->items[i] = ft_valid_atoi(num_list[count - 1]);
+		i++;
+		count--;
 	}
 }
 
@@ -65,9 +81,18 @@ int	main(int argc, char *argv[])
 	t_stack	b;
 	char	**num_list;
 
-	if (argc != 2)
+	ft_printf("argc = %d\n", argc);
+	if (argc < 2)
 		exit(EXIT_FAILURE);
-	num_list = ft_split(argv[1], ' ');
+	else if (argc == 2)
+		num_list = ft_split(argv[1], ' ');
+	else if (argc > 2)
+		num_list = argv[1];
+	int	i;
+	i = 0;
+	while (num_list[i])
+		ft_printf("%s, ", num_list[i++]);
+	ft_printf("\n");
 	generate_stacks(&a, &b, num_list);
 	return (0);
 }
