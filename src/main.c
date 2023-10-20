09c44159dae8fbc400424df5c62714884b7b2ccb
Thais Mina nishimoto <tmina-ni@c2r10p4.42sp.org.br>
2023-10-20 19:03:04 -0300

    temp

--- src/main.c
+++ src/main.c
@@ -6,27 +6,123 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 11:53:26 by tmina-ni          #+#    #+#             */
-/*   Updated: 2023/10/20 14:13:40 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2023/10/20 18:53:19 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-char	**parse_argv(int argc, char *argv[])
+char	**join_args(int	argc, const char *argv[])
 {
-	char	**temp;
 	int	i;
+	char	**num_list;
+	char	*temp;
 
-	i = 0;
+	i = 1;
+	temp = ft_strjoin(argv[i], " ");
 	while (i < argc - 1)
 	{
-		
+		num_list = ft_strjoin(temp, argv[i]);
+		free(temp);
+		temp = ft_strjoin(num_list, " ");
+		free(num_list);
+		i++;
 	}
-	else if (argc == 2)
-		num_list = ft_split(argv[1], ' ');
-	else if (argc > 2)
-		num_list = argv[1];
+	temp[i] = NULL;
 }
+
+//int	ft_count_num(char *s, char c)
+//{
+//	int	index;
+//	int	count;
+//
+//	index = 0;
+//	count = 0;
+//	while (s[index])
+//	{
+//		while (s[index] == c)
+//			index++;
+//		if (s[index] && s[index] != c)
+//			count++;
+//		while (s[index] && s[index] != c)
+//			index++;
+//	}
+//	return (count);
+//}
+//
+//char	**parse_argv(int argc, char *argv[])
+//{
+//	char	**num_list;
+//	int	i;
+//	int	count;
+//
+//	i = 1;
+//	while (i < argc - 1)
+//	{
+//		if (ft_strchr(argv[i], ' ') == NULL)
+//		{
+//			num_list = ft_strdup(&argv[i]);
+//			num_list++;
+//			i++;
+//		}
+//		else
+//		{
+//			count = ft_count_num(argv[i], ' ');
+//			num_list = ft_split(argv[i], ' ');
+//			num_list += count;
+//			i++;
+//		}
+//	}
+//	return (num_list);
+//}
+
+//char	**ft_join_args(char **argv)
+//{
+//	char	*tmp;
+//	char	*str;
+//	char	**matrix;
+//	int		i;
+//
+//	i = 1;
+//	tmp = ft_strdup(" ");
+//	while (argv[i])
+//	{
+//		str = ft_strjoin(tmp, argv[i]);
+//		free(tmp);
+//		tmp = ft_strjoin(str, " \0");
+//		free(str);
+//		i++;
+//	}
+//	matrix = ft_split(tmp, ' ');
+//	free(tmp);
+//	return (matrix);
+//}
+
+
+//char	**parse_argv(int argc, char *argv[])
+//{
+//	char	*temp;
+//	char	*num_list;
+//	int	i;
+//	int	count_num;
+//
+//	num_list = argv[1];
+//	i = 1;
+//	while (i < argc - 1)
+//	{
+//		temp = ft_strjoin(argv[i] " ");
+//		num_list = ft_strjoin(temp, argv[]);
+//		free(temp);
+//		
+//		while (num_list[count_num])
+//			count_num++;
+//	}
+//	ft_printf("joined args: %s\n", num_list);
+////	else if (argc == 2)
+////		num_list = ft_split(argv[1], ' ');
+////	else if (argc > 2)
+////		num_list = argv[1];
+//}
 
 int	ft_valid_atoi(const char *nptr)
 {
@@ -77,22 +173,23 @@ void	generate_stacks(t_stack *a, t_stack *b, char **num_list)
 
 int	main(int argc, char *argv[])
 {
-	t_stack	a;
-	t_stack	b;
+//	t_stack	a;
+//	t_stack	b;
 	char	**num_list;
 
 	ft_printf("argc = %d\n", argc);
 	if (argc < 2)
 		exit(EXIT_FAILURE);
-	else if (argc == 2)
-		num_list = ft_split(argv[1], ' ');
-	else if (argc > 2)
-		num_list = argv[1];
+	num_list = parse_argv(argc, argv);
+//	else if (argc == 2)
+//		num_list = ft_split(argv[1], ' ');
+//	else if (argc > 2)
+//		num_list = argv[1];
 	int	i;
 	i = 0;
 	while (num_list[i])
 		ft_printf("%s, ", num_list[i++]);
 	ft_printf("\n");
-	generate_stacks(&a, &b, num_list);
+//	generate_stacks(&a, &b, num_list);
 	return (0);
 }
